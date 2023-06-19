58ee8092c9b53cf1e95bd84d1e6146a0c1bb8ac9
brygmos <daniel-best@yandex.ru>
2023-06-19 20:59:29 +0300

    in porogress

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,62 @@
+const { Worker, isMainThread } = require('worker_threads');
+const os = require('os');
+
 const performCalculations = async () => {
-    // Write your code here
+    if (isMainThread) {
+        const numCPUs = os.cpus().length;
+        const workers = [];
+        const promises = [];
+
+        for (let i = 0; i < numCPUs; i++) {
+            workers.push(new Worker('./worker.js'));
+            promises.push(
+                new Promise((resolve) => {
+                    workers[i].on('message', ({ status, data }) =>
+                        resolve({
+                            status,
+                            data,
+                        })
+                    );
+                })
+            );
+        }
+
+        let counter = 10;
+
+        await Promise.all(
+            promises.map((promise) => {
+                return promise.then(({ status, data }) => ({
+                    status: status === 'resolved' ? 'resolved' : 'error',
+                    data,
+                }));
+            })
+        );
+
+        console.log('==============================================')
+
+        for (const worker of workers){
+            worker.postMessage({number: counter++});
+        }
+
+        // Wait until all Promises are resolved
+        const resultArray = await Promise.all(promises);
+
+        console.log(resultArray);
+
+    } else {
+        console.log('main thread')
+        // Receive message from main thread
+        // parentPort.on("message", ({ number })=>{
+        //
+        //     try{
+        //         const incrementedNumber = number + 1;
+        //         parentPort.postMessage({status: 'resolved', data: incrementedNumber});
+        //     }catch(error){
+        //         parentPort.postMessage({status: 'error',data: null});
+        //     }
+        // });
+    }
+
 };
 
-await performCalculations();
+(async () => {await performCalculations()})();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,30 @@
+const { parentPort } = require('worker_threads');
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
+let result = null;
+
 const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    console.log('hiiiiiiiiiiii');
+    // parentPort.on('message', (data) => {
+    //     console.log('5555555')
+    //     console.log(data.number)
+    //     result = nthFibonacci(data.number);
+    //     console.log(result)
+    //     parentPort.postMessage({status: 'resolved', data: result});
+    // })
+    parentPort.postMessage({status: 'resolved', data: nthFibonacci(10)});
 };
+
+parentPort.on('message', (data) => {
+    console.log('5555555')
+    console.log(data.number)
+    result = nthFibonacci(data.number);
+    console.log(result)
+    parentPort.postMessage({status: 'resolved', data: result});
+})
+
 
 sendResult();
