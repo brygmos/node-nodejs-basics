e747e9425e4b7f203bd0d8c4d2aa8abcea784916
brygmos <daniel-best@yandex.ru>
2023-06-19 12:59:45 +0300

    refactor: cjsToEsm

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+process.argv.push('--propName', 'value');
+process.argv.push('--propName2', 'value2');
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const parsedArgs = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        parsedArgs.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+    }
+
+    console.log(parsedArgs.join(', '));
 };
 
 parseArgs();
