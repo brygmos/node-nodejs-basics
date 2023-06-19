eeedc80c147bf52d348057e96ff6ce57c3400316
brygmos <daniel-best@yandex.ru>
2023-06-19 13:59:20 +0300

    feat: implement zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
 const decompress = async () => {
-    // Write your code here 
+    const fs = require('fs');
+    const zlib = require('zlib');
+
+    const filePath = './files/fileToCompress.txt';
+
+    const readStream = fs.createReadStream('./files/archive.gz');
+
+    const writeStream = fs.createWriteStream(filePath);
+
+    const gunzipStream = zlib.createGunzip();
+
+    readStream.pipe(gunzipStream).pipe(writeStream);
+
+    await new Promise((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+    });
 };
 
 await decompress();
